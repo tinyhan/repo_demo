c9c08dbd6c23c5efc6a6bdd843ad9555d47d975e
LarcnHan <hangz80@126.com>
2017-08-02 06:02:54 +0800

    demo

---
+++ Hello/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hello;
+/**
+ * Created by PhpStorm.
+ * User: larcn
+ * Date: 08/02/17
+ * Time: 10:40 AM
+ */
+class HelloWorld
+{
+    public function say_hello(){
+        echo "hello world";
+    }
+
+}
